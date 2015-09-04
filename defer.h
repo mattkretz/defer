4ef0e012c4266485034dc7a30ce0936e8af5dd39
Matthias Kretz <kretz@kde.org>
2015-09-04 10:31:13 +0200

    Ensure that the cleanup_function never gets duplicated.
    * Document the internal defer_raii member functions a bit.
    * Declare all copy/move operations as deleted.
    * Use implicit construction on return from defer(F &&) to avoid the
    copy.
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- defer.h
+++ defer.h
@@ -5,9 +5,17 @@ namespace detail
 template <typename F> class defer_raii
 {
 public:
-  defer_raii(const defer_raii &) = default;
-  defer_raii(defer_raii &&) = default;
+  // copy/move construction and any kind of assignment would lead to the cleanup function getting
+  // called twice. We can't have that.
+  defer_raii(defer_raii &&) = delete;
+  defer_raii(const defer_raii &) = delete;
+  defer_raii &operator=(const defer_raii &) = delete;
+  defer_raii &operator=(defer_raii &&) = delete;
+
+  // construct the object from the given callable
   template <typename FF> defer_raii(FF &&f) : cleanup_function(std::forward<FF>(f)) {}
+
+  // when the object goes out of scope call the cleanup function
   ~defer_raii() { cleanup_function(); }
 
 private:
@@ -17,7 +25,7 @@ private:
 
 template <typename F> detail::defer_raii<F> defer(F &&f)
 {
-  return detail::defer_raii<typename std::remove_reference<F>::type>{std::forward<F>(f)};
+  return {std::forward<F>(f)};
 }
 
 #ifdef __COUNTER__
